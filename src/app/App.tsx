342d48809f028555beaaf1799eb1be0280ac5c27
Adrián Maza <adri_mv1@hotmail.com>
2026-07-23 19:18:46 +0200

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,11 @@
 import { useCallback, useState } from "react";
-import { Navigate, Route, Routes } from "react-router";
 import {
   tasks as initialTasks,
   type AddTaskHandler,
   type Task,
 } from "../features/tasks";
-import DashboardPage from "../pages/DashboardPage";
-import NotFoundPage from "../pages/NotFoundPage";
-import ProjectDetailPage from "../pages/ProjectDetailPage";
-import ProjectsPage from "../pages/ProjectsPage";
-import TasksPage from "../pages/TasksPage";
-import AppLayout from "../shared/components/layout/AppLayout";
 import "./App.css";
+import AppRoutes from "./AppRoutes";
 
 function App() {
   const [taskItems, setTaskItems] = useState<Task[]>(initialTasks);
@@ -26,26 +20,7 @@ function App() {
     setTaskItems((currentTasks) => [newTask, ...currentTasks]);
   }, []);
 
-  return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<DashboardPage tasks={taskItems} />} />
-
-        <Route path="dashboard" element={<Navigate to="/" replace />} />
-
-        <Route path="projects" element={<ProjectsPage />} />
-
-        <Route path="projects/:projectId" element={<ProjectDetailPage />} />
-
-        <Route
-          path="tasks"
-          element={<TasksPage tasks={taskItems} onAddTask={handleAddTask} />}
-        />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  );
+  return <AppRoutes tasks={taskItems} onAddTask={handleAddTask} />;
 }
 
 export default App;
