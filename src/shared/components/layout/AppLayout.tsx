e3176795d9517dd8f8298a8cc0ab8f7c52b84a51
Adrián Maza <adri_mv1@hotmail.com>
2026-07-23 18:30:54 +0200

    AppLayout

--- src/shared/components/layout/AppLayout.tsx
+++ src/shared/components/layout/AppLayout.tsx
@@ -1,24 +1,19 @@
-import type { ReactNode } from "react";
-import type { NavigateHandler, ViewName } from "../../model/navigation";
+import { Outlet } from "react-router";
 import AppHeader from "./AppHeader";
 import Sidebar from "./Sidebar";
 import "./AppLayout.css";
 
-type AppLayoutProps = {
-  children: ReactNode;
-  activeView: ViewName;
-  onNavigate: NavigateHandler;
-};
-
-function AppLayout({ children, activeView, onNavigate }: AppLayoutProps) {
+function AppLayout() {
   return (
     <div className="app-layout">
-      <AppHeader onNavigate={onNavigate} />
+      <AppHeader />
 
       <div className="app-layout__body">
-        <Sidebar activeView={activeView} onNavigate={onNavigate} />
+        <Sidebar />
 
-        <main className="app-layout__content">{children}</main>
+        <main className="app-layout__content">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
