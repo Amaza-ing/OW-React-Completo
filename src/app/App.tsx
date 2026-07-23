67f60c978dcdcd5f8f787aae4abe6ce2e04a9c09
Adrián Maza <adri_mv1@hotmail.com>
2026-07-23 19:26:31 +0200

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,11 @@
-import { useCallback, useState } from "react";
-import {
-  tasks as initialTasks,
-  type AddTaskHandler,
-  type Task,
-} from "../features/tasks";
+import { useTasks } from "../features/tasks";
 import "./App.css";
 import AppRoutes from "./AppRoutes";
 
 function App() {
-  const [taskItems, setTaskItems] = useState<Task[]>(initialTasks);
+  const { tasks, addTask } = useTasks();
 
-  const handleAddTask = useCallback<AddTaskHandler>((taskData) => {
-    const newTask: Task = {
-      id: `task-${Date.now()}`,
-      status: "pending",
-      ...taskData,
-    };
-
-    setTaskItems((currentTasks) => [newTask, ...currentTasks]);
-  }, []);
-
-  return <AppRoutes tasks={taskItems} onAddTask={handleAddTask} />;
+  return <AppRoutes tasks={tasks} onAddTask={addTask} />;
 }
 
 export default App;
