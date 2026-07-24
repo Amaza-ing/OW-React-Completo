e50081f859fadfa9272cdfd1bd6a34d32513a8b6
Adrián Maza <adri_mv1@hotmail.com>
2026-07-24 17:42:20 +0200

    tasksReducer

--- src/features/tasks/reducers/tasksReducer.ts
+++ src/features/tasks/reducers/tasksReducer.ts
@@ -1,14 +1,22 @@
 import type { Task } from "../model/task";
 
-type TasksAction = {
-  type: "task/added";
-  payload: Task;
-};
+type TasksAction =
+  | {
+      type: "task/added";
+      payload: Task;
+    }
+  | {
+      type: "tasks/reset";
+      payload: Task[];
+    };
 
 export function tasksReducer(state: Task[], action: TasksAction): Task[] {
   switch (action.type) {
     case "task/added":
       return [action.payload, ...state];
+
+    case "tasks/reset":
+      return action.payload;
 
     default:
       return state;
