62cbcbd2a5b5518c4cbfe15b32658775267f8bf6
Adrián Maza <adri_mv1@hotmail.com>
2026-07-24 17:36:31 +0200

    useTasks

--- src/features/tasks/hooks/useTasks.ts
+++ src/features/tasks/hooks/useTasks.ts
@@ -1,10 +1,15 @@
-import { useCallback, useReducer } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import { tasks as initialTasks } from "../data/tasks";
 import type { AddTaskHandler, Task } from "../model/task";
 import { tasksReducer } from "../reducers/tasksReducer";
+import { loadTasks, saveTasks } from "../storage/tasksStorage";
 
 export function useTasks() {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks, loadTasks);
+
+  useEffect(() => {
+    saveTasks(tasks);
+  }, [tasks]);
 
   const addTask = useCallback<AddTaskHandler>((taskData) => {
     const newTask: Task = {
