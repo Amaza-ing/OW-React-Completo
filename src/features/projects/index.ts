bc8f7efb529de1e6e1f91020444167bf0d3cca7c
Adrián Maza <adri_mv1@hotmail.com>
2026-07-28 16:08:46 +0200

    projects public API

--- src/features/projects/index.ts
+++ src/features/projects/index.ts
@@ -3,6 +3,7 @@ export { default as ProjectCard } from "./components/ProjectCard/ProjectCard";
 export { default as ProjectList } from "./components/ProjectList/ProjectList";
 export { default as ProjectResults } from "./components/ProjectResults/ProjectResults";
 export { projects } from "./data/projects";
+export { GET_PROJECTS_QUERY } from "./graphql/projectsOperations";
 export { useCompleteProjectMutation } from "./hooks/useCompleteProjectMutation";
 export { useProjectSearch } from "./hooks/useProjectSearch";
 
