f25a3f45567434864f47eb4f30df2b39fa81cac0
Adrián Maza <adri_mv1@hotmail.com>
2026-07-28 13:44:30 +0200

    AppProviders

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -1,12 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 import { TasksProvider } from "../features/tasks";
+import { queryClient } from "./queryClient";
 
 type AppProvidersProps = {
   children: ReactNode;
 };
 
 function AppProviders({ children }: AppProvidersProps) {
-  return <TasksProvider>{children}</TasksProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TasksProvider>{children}</TasksProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default AppProviders;
