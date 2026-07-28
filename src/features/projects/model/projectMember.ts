31ff158a0f964421b9c7e0c160abd4c4903706a4
Adrián Maza <adri_mv1@hotmail.com>
2026-07-28 17:43:47 +0200

    projectMember

--- src/features/projects/model/projectMember.ts
+++ src/features/projects/model/projectMember.ts
@@ -1,3 +1,20 @@
+export const projectTeamSourceOptions = [
+  {
+    value: "graphql",
+    label: "GraphQL",
+  },
+  {
+    value: "rest",
+    label: "REST",
+  },
+] as const satisfies readonly {
+  value: string;
+  label: string;
+}[];
+
+export type ProjectTeamSource =
+  (typeof projectTeamSourceOptions)[number]["value"];
+
 export interface ProjectMember {
   id: string;
   name: string;
