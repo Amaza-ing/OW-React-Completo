f9b460b78a62042ad0952e4313b24b0614d59d6c
Adrián Maza <adri_mv1@hotmail.com>
2026-08-01 16:58:06 +0200

    projects public API

--- src/features/projects/index.ts
+++ src/features/projects/index.ts
@@ -1,11 +1,19 @@
 export { completeProject, getProjects } from "./api/projectsApi";
+
 export { default as ProjectCard } from "./components/ProjectCard/ProjectCard";
+
 export { default as ProjectList } from "./components/ProjectList/ProjectList";
+
 export { default as ProjectResults } from "./components/ProjectResults/ProjectResults";
+
 export { default as ProjectTeamSection } from "./components/ProjectTeamSection/ProjectTeamSection";
+
 export { projects } from "./data/projects";
+
 export { useAddProjectMemberMutation } from "./hooks/useAddProjectMemberMutation";
+
 export { useCompleteProjectMutation } from "./hooks/useCompleteProjectMutation";
+
 export { useProjectSearch } from "./hooks/useProjectSearch";
 
 export {
@@ -13,7 +21,7 @@ export {
   projectsQueryOptions,
   useProjectQuery,
   useProjectsQuery,
-  useProjectTeamQuery,
+  useSuspenseProjectTeamQuery,
 } from "./hooks/useProjectsQuery";
 
 export type { Project, ProjectStatus } from "./model/project";
