ff75530b81ecabdee85881ab84ee7a06b6f77539
Adrián Maza <adri_mv1@hotmail.com>
2026-08-01 17:21:24 +0200

    TaskSearch

---
+++ src/features/tasks/components/TaskSearch/TaskSearch.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import type { ChangeEventHandler } from "react";
+
+type TaskSearchProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+function TaskSearch({ value, onChange }: TaskSearchProps) {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    onChange(event.currentTarget.value);
+  };
+
+  return (
+    <label>
+      <span>Buscar por título</span>
+
+      <input
+        type="search"
+        value={value}
+        onChange={handleChange}
+        placeholder="Ej. diseño"
+      />
+    </label>
+  );
+}
+
+export default memo(TaskSearch);
