f95f543a5ecedfcd7c664da7dc7cae68839bdfba
Adrián Maza <adri_mv1@hotmail.com>
2026-08-03 19:33:42 +0200

    PwaControls

--- src/pwa/components/PwaControls.tsx
+++ src/pwa/components/PwaControls.tsx
@@ -1,9 +1,11 @@
 import ConnectionStatus from "./ConnectionStatus";
 import "./PwaControls.css";
+import PwaUpdatePrompt from "./PwaUpdatePrompt";
 
 function PwaControls() {
   return (
     <aside className="pwa-controls" aria-label="Estado de la aplicación">
+      <PwaUpdatePrompt />
       <ConnectionStatus />
     </aside>
   );
