0b13b4fc4673f0a5dc78c0e327ec0abf0667017a
Adrián Maza <adri_mv1@hotmail.com>
2026-08-03 13:42:05 +0200

    useOnlineStatus

---
+++ src/pwa/hooks/useOnlineStatus.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+function getOnlineStatus() {
+  return navigator.onLine;
+}
+
+export function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(getOnlineStatus);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline(true);
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+    };
+
+    window.addEventListener("online", handleOnline);
+
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline;
+}
