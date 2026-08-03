e00b2f9c3a5f2172b6520598fc61f61f5d6960e6
Adrián Maza <adri_mv1@hotmail.com>
2026-08-03 18:49:33 +0200

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import "./other.css";
 import AppRoutes from "./AppRoutes";
 import AppProviders from "./AppProviders";
+import PwaControls from "../pwa/components/PwaControls";
 
 function App() {
   return (
     <AppProviders>
       <AppRoutes />;
+      <PwaControls />
     </AppProviders>
   );
 }
