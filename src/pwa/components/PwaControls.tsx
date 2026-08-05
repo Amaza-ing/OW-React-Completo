bca1e993d81ac77a9cdba6d3fc00757010d28ea2
Adrián Maza <adri_mv1@hotmail.com>
2026-08-05 13:18:08 +0200

    PwaControls

--- src/pwa/components/PwaControls.tsx
+++ src/pwa/components/PwaControls.tsx
@@ -3,12 +3,14 @@ import InstallAppButton from "./InstallAppButton";
 import NotificationControls from "./NotificationControls";
 import "./PwaControls.css";
 import PwaUpdatePrompt from "./PwaUpdatePrompt";
+import ShareAppButton from "./ShareAppButton";
 
 function PwaControls() {
   return (
     <aside className="pwa-controls" aria-label="Estado de la aplicación">
       <PwaUpdatePrompt />
       <NotificationControls />
+      <ShareAppButton />
       <InstallAppButton />
       <ConnectionStatus />
     </aside>
