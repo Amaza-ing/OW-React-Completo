d82c436590bf3fd363cace5df3d502a7bb5e391e
Adrián Maza <adri_mv1@hotmail.com>
2026-08-05 12:56:23 +0200

    dependencies installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 // npm install --save-dev vite-plugin-pwa
 // npm install --save-dev workbox-window
 // npm install firebase
+// npm install @tanstack/react-query-persist-client
+// npm install @tanstack/query-sync-storage-persister
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
