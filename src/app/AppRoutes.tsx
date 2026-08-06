072ea877f03b2b5b8575593b2f7e45ef18687565
Adrián Maza <adri_mv1@hotmail.com>
2026-08-06 11:57:38 +0200

    AppRoutes

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router";
 import DashboardPage from "../pages/DashboardPage";
 import AppLayout from "../shared/components/layout/AppLayout";
 import {
+  ArchitecturePage,
   NotFoundPage,
   ProjectDetailPage,
   ProjectsPage,
@@ -21,6 +22,8 @@ function AppRoutes() {
         <Route path="projects/:projectId" element={<ProjectDetailPage />} />
 
         <Route path="tasks" element={<TasksPage />} />
+
+        <Route path="architecture" element={<ArchitecturePage />} />
 
         <Route path="*" element={<NotFoundPage />} />
       </Route>
