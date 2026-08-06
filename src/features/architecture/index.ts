68789bf60eb9ffc8853a5acbb32e72766a82027d
Adrián Maza <adri_mv1@hotmail.com>
2026-08-06 12:33:08 +0200

    architecture public API

---
+++ src/features/architecture/index.ts
@@ -0,0 +1,20 @@
+export { default as ArchitectureAdvisor } from "./components/ArchitectureAdvisor";
+export { default as ArchitectureOptionCards } from "./components/ArchitectureOptionCards";
+export { default as MigrationPlanPanel } from "./components/MigrationPlanPanel";
+export { default as RenderingImpactTable } from "./components/RenderingImpactTable";
+export { default as SpaSuitabilityPanel } from "./components/SpaSuitabilityPanel";
+
+export { architectureOptions } from "./architectureOptions";
+export type {
+  ArchitectureOption,
+  ArchitectureOptionId,
+} from "./architectureOptions";
+
+export { renderingStrategies } from "./renderingStrategies";
+export type {
+  RenderingStrategy,
+  RenderingStrategyId,
+} from "./renderingStrategies";
+
+export { taskFlowMigrationPlan } from "./migrationPlan";
+export type { MigrationReadiness, MigrationSlice } from "./migrationPlan";
