b4f2b4dc56a2f01a76a2adbfaf4c738b005000dd
Adrián Maza <adri_mv1@hotmail.com>
2026-08-06 12:34:35 +0200

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,14 @@
+import PwaEnhancements from "../pwa/PwaEnhancements";
 import "./App.css";
-import "./other.css";
-import AppRoutes from "./AppRoutes";
 import AppProviders from "./AppProviders";
-import PwaControls from "../pwa/components/PwaControls";
-import PwaTaskBridge from "./PwaTaskBridge";
+import AppRoutes from "./AppRoutes";
+import "./Course2.css";
 
 function App() {
   return (
     <AppProviders>
       <AppRoutes />
-      <PwaTaskBridge />
-      <PwaControls />
+      <PwaEnhancements />
     </AppProviders>
   );
 }
