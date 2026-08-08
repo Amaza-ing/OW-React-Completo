d667ef1741980a4c9c12e684ff609c5da3698224
Adrián Maza <adri_mv1@hotmail.com>
2026-08-08 13:42:22 +0200

    projects public API

---
+++ taskflow-router/app/features/projects/index.ts
@@ -0,0 +1,12 @@
+export { default as ProjectCard } from "./components/ProjectCard";
+export { default as ProjectSearchForm } from "./components/ProjectSearchForm";
+export { default as QuickMemberForm } from "./components/QuickMemberForm";
+export type { MemberFormFeedback } from "./components/QuickMemberForm";
+
+export { projectStatusLabels, projectStatusValues } from "./model/project";
+export type {
+  AddProjectMemberInput,
+  Project,
+  ProjectMember,
+  ProjectStatus,
+} from "./model/project";
