9507fce0c9a5001a9e0795b9c87a36bbb689dcad
Adrián Maza <adri_mv1@hotmail.com>
2026-08-08 13:29:11 +0200

    routes.ts

--- taskflow-router/app/routes.ts
+++ taskflow-router/app/routes.ts
@@ -2,5 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("projects", "routes/projects.tsx"),
+  route("projects", "routes/projects/layout.tsx", [
+    index("routes/projects/index.tsx"),
+    route(":projectId", "routes/projects/detail.tsx"),
+  ]),
 ] satisfies RouteConfig;
