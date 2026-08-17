bbc8162b9c3f3f25f92d22b366991faca6c8dd36
Adrián Maza <adri_mv1@hotmail.com>
2026-08-17 18:09:37 +0200

    AppProviders

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { MotionConfig } from "motion/react";
 import type { ReactNode } from "react";
 import { TasksProvider } from "../features/tasks";
 import { queryClient } from "./queryClient";
@@ -9,9 +10,11 @@ type AppProvidersProps = {
 
 function AppProviders({ children }: AppProvidersProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TasksProvider>{children}</TasksProvider>
-    </QueryClientProvider>
+    <MotionConfig reducedMotion="user">
+      <QueryClientProvider client={queryClient}>
+        <TasksProvider>{children}</TasksProvider>
+      </QueryClientProvider>
+    </MotionConfig>
   );
 }
 
