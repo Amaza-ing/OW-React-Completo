df73bf451929bf04f516c63810aeab63be5816ab
Adrián Maza <adri_mv1@hotmail.com>
2026-08-17 17:54:13 +0200

    motion install

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-// npm install react-router
-// npm install @tanstack/react-query
-// npm install --save-dev graphql graphql-yoga
+// npm install motion
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
