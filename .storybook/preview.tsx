57c7eb7dfcc50dfd92fcc958a95aec72db6f9707
Adrián Maza <adri_mv1@hotmail.com>
2026-08-17 12:01:31 +0200

    .stroybook/

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,20 +1,17 @@
 import type { Preview } from "@storybook/react-vite";
+import "../src/index.css";
+import "../src/app/App.css";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <div className="page">
+        <Story />
+      </div>
+    ),
+  ],
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: "todo",
-    },
+    layout: "padded",
   },
 };
 
