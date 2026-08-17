ca9c251f07dbf7d7531cfcf4b3665ec9f836fd57
Adrián Maza <adri_mv1@hotmail.com>
2026-08-17 19:52:01 +0200

    lazyPages

--- src/app/lazyPages.ts
+++ src/app/lazyPages.ts
@@ -1,11 +1,11 @@
 import { lazy } from "react";
 
-export const ProjectsPage = lazy(() => import("../pages/ProjectsPage"));
+export const ProjectsPage = lazy(() => import("@/pages/ProjectsPage"));
 
 export const ProjectDetailPage = lazy(
-  () => import("../pages/ProjectDetailPage"),
+  () => import("@/pages/ProjectDetailPage"),
 );
 
-export const TasksPage = lazy(() => import("../pages/TasksPage"));
+export const TasksPage = lazy(() => import("@/pages/TasksPage"));
 
-export const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
+export const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
