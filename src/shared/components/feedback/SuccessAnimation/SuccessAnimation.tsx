02c7b04cb8b0b3df18140744ec368e4193226bad
Adrián Maza <adri_mv1@hotmail.com>
2026-08-17 18:11:32 +0200

    SuccessAnimation

--- src/shared/components/feedback/SuccessAnimation/SuccessAnimation.tsx
+++ src/shared/components/feedback/SuccessAnimation/SuccessAnimation.tsx
@@ -1,14 +1,17 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { useReducedMotion } from "motion/react";
 import "./SuccessAnimation.css";
 
 function SuccessAnimation() {
+  const shouldReduceMotion = useReducedMotion();
+
   return (
     <span className="success-animation" aria-hidden="true">
       <DotLottieReact
         className="success-animation__player"
         src="/animations/task-success.json"
-        autoplay
-        loop={true}
+        autoplay={!shouldReduceMotion}
+        loop={false}
       />
 
       <span className="success-animation__check">✓</span>
