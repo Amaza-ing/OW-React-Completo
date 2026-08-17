a22098e784797427c72d5c34c034cb221787e0eb
Adrián Maza <adri_mv1@hotmail.com>
2026-08-17 14:37:40 +0200

    commands

---
+++ cypress/support/commands.ts
@@ -0,0 +1,16 @@
+/* global Cypress, cy */
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      dataCy(value: string): Chainable<JQuery<HTMLElement>>;
+    }
+  }
+}
+
+Cypress.Commands.add("dataCy", (value: string) => {
+  return cy.get(`[data-cy="${value}"]`);
+});
+
+export {};
